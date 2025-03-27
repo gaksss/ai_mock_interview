e45d480e2fec76be9701c86bb4a242b019b75072
gaksss <clement.carlier2@gmail.com>
2025-03-27 15:51:01 +0100

    setup code error

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import { Control, Controller, FieldValues, Path } from "react-hook-form";
+import { Controller, Control, FieldValues, Path } from "react-hook-form";
+
 import {
-  FormControl,
-  FormDescription,
   FormItem,
   FormLabel,
+  FormControl,
   FormMessage,
-} from "./ui/form";
-import { Input } from "./ui/input";
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 interface FormFieldProps<T extends FieldValues> {
   control: Control<T>;
   name: Path<T>;
   label: string;
   placeholder?: string;
-  type?: "text" | "email" | "password" | "file";
+  type?: "text" | "email" | "password";
 }
 
 const FormField = <T extends FieldValues>({
@@ -23,25 +22,27 @@ const FormField = <T extends FieldValues>({
   label,
   placeholder,
   type = "text",
-}: FormFieldProps<T>) => (
-  <Controller
-    control={control}
-    name={name}
-    render={({ field }) => (
-      <FormItem>
-        <FormLabel>{label}</FormLabel>
-        <FormControl>
-          <Input
-            className="input"
-            placeholder={placeholder}
-            type={type}
-            {...field}
-          />
-        </FormControl>
-        <FormMessage />
-      </FormItem>
-    )}
-  />
-);
+}: FormFieldProps<T>) => {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel className="label">{label}</FormLabel>
+          <FormControl>
+            <Input
+              className="input"
+              type={type}
+              placeholder={placeholder}
+              {...field}
+            />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
 
 export default FormField;
---
+++ firebase/admin.ts
@@ -0,0 +1,29 @@
+import { initializeApp, getApps, cert } from "firebase-admin/app";
+import { Auth, getAuth } from "firebase-admin/auth";
+import { Firestore, getFirestore } from "firebase-admin/firestore";
+
+interface FirebaseAdminConfig {
+  auth: Auth;
+  db: Firestore;
+}
+
+function getFirebaseAdmin(): FirebaseAdminConfig {
+  const apps = getApps();
+
+  if (!apps.length) {
+    initializeApp({
+      credential: cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      }),
+    });
+  }
+
+  return {
+    auth: getAuth(),
+    db: getFirestore(),
+  };
+}
+
+export const { auth, db } = getFirebaseAdmin();
